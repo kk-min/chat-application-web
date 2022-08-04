fbf6917ee44ebad83c5096de980bf330860722e9
MINK <76023265+mink0003@users.noreply.github.com>
2022-08-04 15:02:00 +0800

    Added theme to index.js

--- chat_web/src/index.js
+++ chat_web/src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles/";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export const themeOptions = createTheme({
+	palette: {
+		type: "light",
+		primary: {
+			main: "rgba(224,103,104,0.67)",
+		},
+		secondary: {
+			main: "#0099f5",
+		},
+		background: {
+			default: "#cff7f3",
+		},
+	},
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<ThemeProvider theme={themeOptions}>
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
