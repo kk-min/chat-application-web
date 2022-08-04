8874b04f14029333a6dd6c730a74d3baf715990c
MINK <76023265+mink0003@users.noreply.github.com>
2022-08-04 18:00:54 +0800

    Added ChatScreen (In progress)

--- chat_web/src/App.js
+++ chat_web/src/App.js
@@ -3,17 +3,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routers, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ChatScreen from "./pages/ChatScreen";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/chat' element={<ChatScreen />}></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
