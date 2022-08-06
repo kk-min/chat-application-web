5297042c6248a86bfbd364c5f329b01763c4c9df
mink0003 <76023265+mink0003@users.noreply.github.com>
2022-08-06 18:53:07 +0800

    Set up ChatScreen/ChatWindow
    Implementing ChatWindow to contain ChatBubble (In Progress)
    
    Added send functionality in ChatScreen.

--- chat_web/src/App.js
+++ chat_web/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routers, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import ChatScreen from "./pages/ChatScreen";
+import Debug from "./pages/Debug.jsx";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/chat" element={<ChatScreen />}></Route>
+          <Route path="/chat" element={<ChatScreen />} />
+          <Route path="/debug" element={<Debug />} />
         </Routes>
       </BrowserRouter>
     </div>
