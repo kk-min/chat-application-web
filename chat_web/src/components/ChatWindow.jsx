811dd09ac7caa28d7c8b70d3615eb3df7a4bf939
mink0003 <76023265+mink0003@users.noreply.github.com>
2022-08-06 18:53:07 +0800

    Set up firestore in ChatScreen, currently in testing phase

--- chat_web/src/components/ChatWindow.jsx
+++ chat_web/src/components/ChatWindow.jsx
@@ -1,12 +1,8 @@
 import React from "react";
-import { TextField, Grid, Button, Box } from "@mui/material";
+import { TextField, Grid, Button, Box, ListItem } from "@mui/material";
 import { VariableSizeList } from "react-window";
+import ChatBubble from "./ChatBubble";
 
 export default function ChatWindow() {
-  return (
-    <VariableSizeList
-      itemCount={10000}
-      itemSize={getChatBubbleSize}
-    ></VariableSizeList>
-  );
+  return;
 }
