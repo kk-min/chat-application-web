03d0fceaf7ecb956141e0292f132b7af58e318a5
MINK <76023265+mink0003@users.noreply.github.com>
2022-08-22 15:25:17 +0800

    Fix formatting for ChatBox component

--- chat_web/src/App.js
+++ chat_web/src/App.js
@@ -7,30 +7,29 @@ import ChatScreen from "./pages/ChatScreen";
 import Welcome from "./pages/Welcome";
 import Debug from "./pages/Debug";
 
-
 function App() {
-  const [userName, setUserName] = useState();
+	const [userName, setUserName] = useState();
 
-  useEffect(() =>{
-    document.body.style.overflow = "hidden";
-  })
+	useEffect(() => {
+		document.body.style = "background: #e0dffa;";
+		document.body.style.overflow = "hidden";
+	});
 
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/name"
-            element={<NameScreen setUserName={setUserName} />}
-          />
-          <Route path="/chat" element={<ChatScreen userName={userName} />} />
-          <Route path="/welcome" element={<Welcome/>}/>
-          <Route path="/debug" element={<Debug />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route
+						path='/chat'
+						element={<ChatScreen userName={"LQY"} />}
+					/>
+					<Route path='/welcome' element={<Welcome />} />
+					<Route path='/debug' element={<Debug />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
--- chat_web/src/pages/ChatScreen.jsx
+++ chat_web/src/pages/ChatScreen.jsx
@@ -7,92 +7,100 @@ import ChatWindow from "../components/ChatWindow";
 import { useNavigate } from "react-router-dom";
 import EscapeDialog from "../components/EscapeDialog";
 
-
-
 export default function ChatScreen(props) {
-  const [loggedIn, setLoggedIn] = useState(null);
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [dialogOption, setDialogOption] = useState(false);
-  const navigate = useNavigate();
+	const [loggedIn, setLoggedIn] = useState(null);
+	const [dialogOpen, setDialogOpen] = useState(false);
+	const [dialogOption, setDialogOption] = useState(false);
+	const navigate = useNavigate();
 
-  const handleClose = (value) =>{
-    setDialogOpen(false);
-    setDialogOption(value);
-  }
+	const handleClose = (value) => {
+		setDialogOpen(false);
+		setDialogOption(value);
+	};
 
-  const handleEsc = (event) =>{
-    if(event.key == "Escape"){
-      setDialogOpen(true);
-    }
-  }
+	const handleEsc = (event) => {
+		if (event.key == "Escape") {
+			setDialogOpen(true);
+		}
+	};
 
-  useEffect(() => {
-    if(dialogOption === true){
-      navigate("/");
-    }
-    //Set up observer on user authentication:
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        console.log("Authorization granted.");
-        setLoggedIn(true);
-      } else {
-        // User is signed out
-        console.log("Not authorized.");
-        setLoggedIn(false);
-      }
-    });
+	useEffect(() => {
+		if (dialogOption === true) {
+			navigate("/");
+		}
+		//Set up observer on user authentication:
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// User is signed in, see docs for a list of available properties
+				// https://firebase.google.com/docs/reference/js/firebase.User
+				console.log("Authorization granted.");
+				setLoggedIn(true);
+			} else {
+				// User is signed out
+				console.log("Not authorized.");
+				setLoggedIn(false);
+			}
+		});
 
-    //Set up observer on keypress:
-    document.addEventListener('keydown', handleEsc);
+		//Set up observer on keypress:
+		document.addEventListener("keydown", handleEsc);
 
-    // Don't forget to clean up
-    return function cleanup() {
-      document.removeEventListener('keydown', handleEsc);
-    }
+		// Don't forget to clean up
+		return function cleanup() {
+			document.removeEventListener("keydown", handleEsc);
+		};
+	}, [loggedIn, dialogOption]);
 
-  }, [loggedIn, dialogOption]);
-
-  return loggedIn ? (
-    <Grid
-      container
-      justifyContent="flex-end"
-      p={2}
-      sx={{ minHeight: "100vh", bgcolor: "background.default" }}
-    > 
-      <Grid container sx={{ justifyContent: "flex-start", alignItems: "flex-start"}} xs={12}>
-        <ChatWindow userName={props.userName}/>
-      </Grid>
-        <Grid container xs={12} sx={{justifyContent: "flex-start",}}>
-          <ChatBox userName={props.userName} />
-      </Grid>
-      <EscapeDialog onClose={handleClose} open={dialogOpen}/>
-    </Grid>
-  ) : loggedIn == null ? (
-    <Grid
-      container
-      spacing={0}
-      sx={{ minHeight: "100vh", bgcolor: "background.default" }}
-    ></Grid>
-  ) : (
-    <Grid
-      container
-      spacing={0}
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: "100vh", bgcolor: "background.default" }}
-    >
-      <Typography
-        id="title"
-        variant="h3"
-        sx={{
-          padding: 3,
-          fontWeight: "bold",
-        }}
-      >
-        Nice Try
-      </Typography>
-    </Grid>
-  );
+	return loggedIn ? (
+		<Grid
+			container
+			justifyContent='flex-end'
+			p={0}
+			sx={{ minHeight: "100vh", bgcolor: "background.default" }}
+		>
+			<Grid
+				container
+				sx={{ justifyContent: "flex-start", alignItems: "flex-start" }}
+				xs={12}
+			>
+				<ChatWindow userName={props.userName} />
+			</Grid>
+			<Grid
+				container
+				xs={12}
+				sx={{ alignItems: "center" }}
+				mb={5}
+				ml={2}
+				mr={1}
+			>
+				<ChatBox userName={props.userName} />
+			</Grid>
+			<EscapeDialog onClose={handleClose} open={dialogOpen} />
+		</Grid>
+	) : loggedIn == null ? (
+		<Grid
+			container
+			spacing={0}
+			sx={{ minHeight: "100vh", bgcolor: "background.default" }}
+		></Grid>
+	) : (
+		<Grid
+			container
+			spacing={0}
+			alignItems='center'
+			justifyContent='center'
+			sx={{ minHeight: "100vh", bgcolor: "background.default" }}
+		>
+			<Typography
+				id='title'
+				variant='h3'
+				sx={{
+					padding: 3,
+					fontWeight: "bold",
+				}}
+			>
+				Nice Try
+			</Typography>
+		</Grid>
+	);
 }
